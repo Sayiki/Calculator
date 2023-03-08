bfe5052e59715f306c59922157ac3617438fd391
Sayiki <arzaqajradika1@gmail.com>
2023-03-08 17:09:42 +0700

    dabloons

--- mod3_1302210096/mod3_1302210096/Form1.cs
+++ mod3_1302210096/mod3_1302210096/Form1.cs
@@ -6,135 +6,143 @@ namespace mod3_1302210096
         {
             InitializeComponent();
         }
-
+        int a;
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button1.Text;
+                textBox1.Text = "1";
             }
             else
             {
-                textBox1.Text += button1.Text;
+                textBox1.Text += "1";
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button2.Text;
+                textBox1.Text = "2";
             }
             else
             {
-                textBox1.Text += button2.Text;
+                textBox1.Text += "2";
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button3.Text;
+                textBox1.Text = "3";
             }
             else
             {
-                textBox1.Text += button3.Text;
+                textBox1.Text += "3";
             }
         }
 
-        private void button11_Click(object sender, EventArgs e)
-        {
-            textBox1.Text += button11.Text;
-        }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button4.Text;
+                textBox1.Text = "4";
             }
             else
             {
-                textBox1.Text += button4.Text;
+                textBox1.Text += "4";
             }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button5.Text;
+                textBox1.Text = "5";
             }
             else
             {
-                textBox1.Text += button5.Text;
+                textBox1.Text += "5";
             }
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button6.Text;
+                textBox1.Text = "6";
             }
             else
             {
-                textBox1.Text += button6.Text;
+                textBox1.Text += "6";
             }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button7.Text;
+                textBox1.Text = "7";
             }
             else
             {
-                textBox1.Text += button7.Text;
+                textBox1.Text += "7";
             }
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button8.Text;
+                textBox1.Text = "8";
             }
             else
             {
-                textBox1.Text += button8.Text;
+                textBox1.Text += "8";
             }
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button9.Text;
+                textBox1.Text = "9";
             }
             else
             {
-                textBox1.Text += button9.Text;
+                textBox1.Text += "9";
             }
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            if (textBox1 == null)
+            if (textBox1.Text == null)
             {
-                textBox1.Text = button10.Text;
+                textBox1.Text = "0";
             }
             else
             {
-                textBox1.Text += button10.Text;
+                textBox1.Text += "0";
             }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int tempa = int.Parse(textBox1.Text);
+            a += tempa;
+  
+            textBox1.Text = null;
+
+
+
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-
+            String b = Convert.ToString(a);
+            textBox1.Text = b;
         }
     }
 }
