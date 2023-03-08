95b9e55588999fa41869b6578cc13390087d1069
Sayiki <arzaqajradika1@gmail.com>
2023-03-08 16:19:32 +0700

    First commit

---
+++ mod3_1302210096/mod3_1302210096/Form1.cs
@@ -0,0 +1,140 @@
+namespace mod3_1302210096
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button1.Text;
+            }
+            else
+            {
+                textBox1.Text += button1.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button2.Text;
+            }
+            else
+            {
+                textBox1.Text += button2.Text;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button3.Text;
+            }
+            else
+            {
+                textBox1.Text += button3.Text;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += button11.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button4.Text;
+            }
+            else
+            {
+                textBox1.Text += button4.Text;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button5.Text;
+            }
+            else
+            {
+                textBox1.Text += button5.Text;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button6.Text;
+            }
+            else
+            {
+                textBox1.Text += button6.Text;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button7.Text;
+            }
+            else
+            {
+                textBox1.Text += button7.Text;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button8.Text;
+            }
+            else
+            {
+                textBox1.Text += button8.Text;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button9.Text;
+            }
+            else
+            {
+                textBox1.Text += button9.Text;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = button10.Text;
+            }
+            else
+            {
+                textBox1.Text += button10.Text;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
